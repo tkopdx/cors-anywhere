dc9c271a6888d66139eedad6d1ea181076a36050
tkopdx <63491466+tkopdx@users.noreply.github.com>
2020-10-06 21:34:05 +0900

    updated whitelist

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var port = process.env.PORT || 8080;
 // immediate abuse (e.g. denial of service). If you want to block all origins except for some,
 // use originWhitelist instead.
 var originBlacklist = parseEnvList(process.env.CORSANYWHERE_BLACKLIST);
-var originWhitelist = ['https://ey-go-fc9b1.web.app', 'http://localhost:3000', 'https://young-shore-56702.herokuapp.com/'];
+var originWhitelist = ['https://ey-go-fc9b1.web.app', 'http://localhost:3000', 'https://picture-flow.herokuapp.com'];
 function parseEnvList(env) {
   if (!env) {
     return [];
